30ba5efa585e01130d337c8d17144317a32f6e0a
GitUserRavindu <105047448+GitUserRavindu@users.noreply.github.com>
2024-06-09 00:57:58 +0530

    Minor Fixes are done.

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -25,6 +25,8 @@ public class LibraryManagementSystem implements Serializable {
     public void addNewBook(String title, String author, String ISBN) {
         Book newBook = new Book(title, author, ISBN);
         this.bookList.add(newBook);
+        System.out.println("New book added");
+        System.out.println();
     }
 
     public void updateBookInfo(int id, String title, String author, String ISBN) {
@@ -32,71 +34,89 @@ public class LibraryManagementSystem implements Serializable {
         book.setTitle(title);
         book.setAuthor(author);
         book.setISBN(ISBN);
+        System.out.println("Book info updated");
     }
 
     public boolean searchBookByISBN(String ISBN) {
         for (Book book : this.bookList) {
             if (book.getISBN().equals(ISBN)) {
+                System.out.println("Book found");
                 return true;
             }
         }
+        System.out.println("Book not found");
         return false;
     }
 
     public int getIndexOfBookByISBN(String ISBN) {
         for (Book book : this.bookList) {
             if (book.getISBN().equals(ISBN)) {
-                return bookList.indexOf(book);
+                int idx = this.bookList.indexOf(book);
+                System.out.println("Book found. Index: " + idx);
+                return idx;
             }
         }
+        System.out.println("Book not found");
         return -1; // Book not found
     }
 
     public boolean searchBookByTitle(String title) {
         for (Book book : this.bookList) {
             if (book.getTitle().equals(title)) {
+                System.out.println("Book found");
                 return true;
             }
         }
+        System.out.println("Book not found");
         return false;
     }
 
     public int getIndexOfBookByTitle(String title) {
         for (Book book : this.bookList) {
             if (book.getTitle().equals(title)) {
-                return bookList.indexOf(book);
+                int idx = this.bookList.indexOf(book);
+                System.out.println("Book found. Index: " + idx);
+                return idx;
             }
         }
+        System.out.println("Book not found");
         return -1;
     }
 
     // Member Management
-    public void addNewMember(String name, int id, int age) {
+    public void addNewMember(String name, int age) {
         Member newMember = new Member(name, age);
         this.memberList.add(newMember);
+        System.out.println("New member added");
     }
 
     public void updateMemberInfo(int id, String name, int age) {
         Member member = this.memberList.get(id);
         member.setName(name);
         member.setAge(age);
+        System.out.println("Member info updated");
     }
 
     public boolean searchMemberByName(String name) {
         for (Member member : this.memberList) {
             if (member.getName().equals(name)) {
+                System.out.println("Member found");
                 return true;
             }
         }
+        System.out.println("Member not found");
         return false;
     }
 
     public int getIndexOfMemberByName(String name) {
         for (Member member : this.memberList) {
             if (member.getName().equals(name)) {
-                return memberList.indexOf(member);
+                int idx = this.memberList.indexOf(member);
+                System.out.println("Member found. Index: " + idx);
+                return idx;
             }
         }
+        System.out.println("Member not found");
         return -1;
     }
 
@@ -110,7 +130,7 @@ public class LibraryManagementSystem implements Serializable {
                 borrowingBook.setAvailable(false);
                 borrower.addBorrowedBook(borrowingBook);
                 BorrowRecord record = new BorrowRecord(borrowingBook, borrower, LocalDate.now());
-                System.out.println("Borrowing Recording has successfully been created.");
+                System.out.println("Borrowing Record has successfully been created.");
                 this.borrowRecordList.add(record);
             }
             else {
@@ -131,28 +151,47 @@ public class LibraryManagementSystem implements Serializable {
         borrower.removeBorrowedBook(borrowingBook);
         borrowingBook.setAvailable(true);
         record.setReturnDate(LocalDate.now());
+        System.out.println("Returning Book has successfully been returned.");
     }
 
     // Display Management
     public void displayBooks() {
+        System.out.println();
+        System.out.println("Book list:");
+        System.out.format("%5s%20s%20s%20s%20s", "ID", "Title", "Author", "ISBN", "Availability");
+        System.out.println();
+        System.out.println("-------------------------------------------------------------------------------------");
         for (Book book : this.bookList) {
-            System.out.format("%5d %20s %20s %20s %5b", book.getBookId(), book.getTitle(), book.getAuthor(), book.getISBN(), book.isAvailable());
+            System.out.format("%5d%20s%20s%20s%20b", book.getBookId(), book.getTitle(), book.getAuthor(), book.getISBN(), book.isAvailable());
             System.out.println();
         }
+        System.out.println();
     }
 
     public void displayMembers() {
+        System.out.println();
+        System.out.println("Member list:");
+        System.out.format("%5s%20s%20s%20s", "ID", "Name", "Age", "#BorrowedBooks");
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------");
         for (Member member : this.memberList) {
-            System.out.format("%5d %20s %5d %5d", member.getMemberId(), member.getName(), member.getAge(), member.getNumberOfBorrowedBooks());
+            System.out.format("%5d%20s%20d%20d", member.getMemberId(), member.getName(), member.getAge(), member.getNumberOfBorrowedBooks());
             System.out.println();
         }
+        System.out.println();
     }
 
     public void displayBorrowedRecords() {
+        System.out.println();
+        System.out.println("Borrowed record list:");
+        System.out.format("%5s%20s%20s%20s%20s", "ID", "Title", "Borrower", "Borrowed Date", "Returned Date");
+        System.out.println();
+        System.out.println("-------------------------------------------------------------------------------------");
         for (BorrowRecord record : this.borrowRecordList) {
-            System.out.format("%5d %20s %20s %20s %20s", record.getRecordId(), record.getBook().getTitle(), record.getMember().getName(), record.getBorrowDate(), record.getReturnDate());
+            System.out.format("%5d%20s%20s%20s%20s", record.getRecordId(), record.getBook().getTitle(), record.getMember().getName(), record.getBorrowDate(), record.getReturnDate());
             System.out.println();
         }
+        System.out.println();
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,32 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Library Management System");
+
+        LibraryManagementSystem lms = new LibraryManagementSystem();
+
+        lms.addNewBook("TestingTitle1", "TestingAuthor1", "ISBN01");
+        lms.addNewBook("TestingTitle2", "TestingAuthor2", "ISBN02");
+        lms.addNewBook("TestingTitle3", "TestingAuthor3", "ISBN03");
+
+        lms.addNewMember("TestingMember1", 18);
+        lms.addNewMember("TestingMember2", 18);
+        lms.addNewMember("TestingMember3", 18);
+
+        lms.displayBooks();
+        lms.displayMembers();
+
+        lms.addNewBorrowRecord("TestingTitle1", 2);
+        lms.addNewBorrowRecord("TestingTitle2", 2);
+        lms.addNewBorrowRecord("TestingTitle2", 2);
+
+        lms.displayBooks();
+        lms.displayMembers();
+        lms.displayBorrowedRecords();
+
+        lms.returningBook(1);
+
+        lms.displayBooks();
+        lms.displayMembers();
+        lms.displayBorrowedRecords();
     }
 }
