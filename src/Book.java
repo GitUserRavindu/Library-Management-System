fded9c027b461828b9825950081e4bc920088337
GitUserRavindu <105047448+GitUserRavindu@users.noreply.github.com>
2024-06-08 20:53:59 +0530

    Book class created

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,29 @@
-public class Book {
+import java.io.Serializable;
+
+public class Book implements Serializable {
+    // Serialization UID
+    private static final long serialVersionUID = 1L;
+
+    // Attributes
+    private String title;
+    private String author;
+    private String ISBN;
+
+    // Constructor
+    public Book(String title, String author, String ISBN) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+    }
+
+    // Getters
+    public String getTitle() { return this.title;}
+    public String getAuthor() { return this.author;}
+    public String getISBN() { return this.ISBN;}
+
+    // Setters
+    public void setTitle(String title) { this.title = title;}
+    public void setAuthor(String author) { this.author = author;}
+    public void setISBN(String ISBN) { this.ISBN = ISBN;}
+
 }
